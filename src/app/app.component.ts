1f73e34c5dce36bbb171720f4c707859503c025d
ШАХРУХ <132030072+rabiulawal1422@users.noreply.github.com>
2024-10-04 20:56:45 +0900

    page-template-basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { SenderComponentComponent } from './sender-component/sender-component.co
 import { RecieverComponentComponent } from './reciever-component/reciever-component.component';
 import { RegisterComponent } from './register/register.component';
 import { UsersListComponent } from './users-list/users-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SenderComponentComponent, RecieverComponentComponent, RegisterComponent, UsersListComponent],
+  imports: [RouterOutlet, SenderComponentComponent,HeaderComponent, RecieverComponentComponent, RegisterComponent, UsersListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
