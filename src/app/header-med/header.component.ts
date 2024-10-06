14e7cb29d74fe47b72b91caa3c631014e6935a43
ШАХРУХ <132030072+rabiulawal1422@users.noreply.github.com>
2024-10-06 18:53:19 +0900

    until_tasks_from_JS

--- src/app/header-med/header.component.ts
+++ src/app/header-med/header.component.ts
@@ -1,12 +1,40 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
+// const UpperCaseMenuItems = menuItems.map(
+//   (item) => {
+//     return item.toUpperCase
+//   }
+// )
+// console.log(UpperCaseMenuItems)
+// const itemsCycle = menuItems.forEach((el) => {
+//   return el
+// })
+
+let changeText;
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgIf,RouterLink, NgFor],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  isUpperCase = true;
 
+  menuItems = ['Home', 'Doctors', 'Services', 'Pages', 'Blogs', 'Contact Us'];
+
+  changeMenutext() {
+    this.menuItems = this.menuItems.map((item) =>
+      this.isUpperCase ? item.toUpperCase() : item.toLowerCase()
+    );
+    this.isUpperCase = !this.isUpperCase;
+  }
+
+  isShowFaq = true;
+
+  readonly headerItem = 'About';
+
+  // showInOne = itemsCycle;
 }
